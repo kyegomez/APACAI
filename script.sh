2957aa18a91404d5407a63f7ef3e14f37c54e2d6
Kye <kye@apacmediasolutions.com>
2023-09-14 22:47:25 -0400

    renamed

--- script.sh
+++ script.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Specify the path to the "docs" folder
-docs_folder="docs/"
+docs_folder="./docs"
 
 # Check if the "docs" folder exists
 if [ -d "$docs_folder" ]; then
-  # Navigate to the "docs" folder
-  cd "$docs_folder"
+  # Use the find command to locate all "README.md" files recursively
+  # and rename them to "index.md"
+  find "$docs_folder" -type f -name "README.md" -execdir mv {} index.md \;
 
-  # Find all files named "README.md" and rename them to "index.md"
-  find . -type f -name "README.md" -exec rename 's/README.md/index.md/' {} \;
-
-  echo "Renamed all README.md files to index.md in the 'docs' folder."
+  echo "Renamed all README.md files to index.md in the 'docs' folder and its subdirectories."
 else
   echo "The 'docs' folder does not exist."
 fi
